5bb09852f1e514a0cc695858285bc40ac6915292
tylerlybb <tylerlybb@gmail.com>
2016-08-18 05:53:08 -0600

    Beginning Unity project

---
+++ Mahindi/Assets/JoesScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoesScript : MonoBehaviour {
+	public Transform target;
+	public float smoothTime = 0.3f;
+	public Transform aimTarget;
+	private Vector3 velocity = Vector3.zero;
+	void Update() {
+		transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+		transform.LookAt(aimTarget);
+	}
+}
---
+++ Mahindi/Assets/TargetSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSwitcher : MonoBehaviour {
+public Transform target1;
+public Transform target2;
+
+public void Switch() {
+	JoesScript follower = GetComponent<JoesScript>();
+	if (follower == null) Debug.LogError("Hey, where's the ExampleClass?!?");
+
+	if (follower.target == target1) follower.target = target2;
+	else follower.target = target1;
+	}
+}
