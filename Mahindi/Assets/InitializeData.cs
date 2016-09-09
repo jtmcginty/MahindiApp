c236db870328add3760619b5bdadc7064ab07a81
jtmcginty <jtmcginty16@gmail.com>
2016-09-08 22:25:53 -0500

    Prepping for data with buttons. Changed layout.

--- Mahindi/Assets/InitializeData.cs
+++ Mahindi/Assets/InitializeData.cs
@@ -6,6 +6,18 @@ using System.Collections.Generic;
 public class InitializeData: MonoBehaviour
 {
 
+	public string[] dataDAP;
+	public string[] dataCAN;
+	public string[] dataL;
+	public string[] dataPH;
+	public string[] yieldTotal;
+	int indexDAP = 0;
+	int indexCAN = 0;
+	int indexL = 0;
+	int indexPH = 0;
+	int indexYield = 0;
+
+
 	public void getData(){
 		StreamReader reader = File.OpenText("/Users/Jack/Documents/Summary_test.csv");
 		string line;
@@ -28,11 +40,11 @@ public class InitializeData: MonoBehaviour
 			listYieldTotal.Add(lineYield);
 		}
 
-		string[] dataDAP = listDataDAP.ToArray ();
-		string[] dataCAN = listDataCAN.ToArray ();
-		string[] dataL = listDataL.ToArray ();
-		string[] dataPH = listDataPH.ToArray ();
-		string[] yieldTotal = listYieldTotal.ToArray ();
+		dataDAP = listDataDAP.ToArray ();
+		dataCAN = listDataCAN.ToArray ();
+		dataL = listDataL.ToArray ();
+		dataPH = listDataPH.ToArray ();
+		yieldTotal = listYieldTotal.ToArray ();
 
 		System.Console.WriteLine ("DAP:");
 		foreach (string s in dataDAP) {
@@ -40,26 +52,74 @@ public class InitializeData: MonoBehaviour
 		}
 		System.Console.WriteLine ("CAN:");
 		foreach (string s in dataCAN) {
-			System.Console.WriteLine (s);
+			//System.Console.WriteLine (s);
 			Debug.Log (s);
 		}
 		System.Console.WriteLine ("L:");
 		foreach (string s in dataL) {
-			System.Console.WriteLine (s);
+			//System.Console.WriteLine (s);
 			Debug.Log (s);
 		}
 		System.Console.WriteLine ("PH:");
 		foreach (string s in dataPH) {
-			System.Console.WriteLine (s);
+			//System.Console.WriteLine (s);
 			Debug.Log (s);
 		}
 		System.Console.WriteLine ("Yield:");
 		foreach (string s in yieldTotal) {
-			System.Console.WriteLine (s);
+			//System.Console.WriteLine (s);
 			Debug.Log (s);
 		}
 	}
 
+	public string getDAP(int index){
+		return dataDAP [index];
+	}
+	public string getCAN(int index){
+		return dataCAN [index];
+	}
+	public string getL(int index){
+		return dataL [index];
+	}
+	public string getPH(int index){
+		return dataPH [index];
+	}
+	public string getYield(int index){
+		return yieldTotal [index];
+	}
+
+
+
+	public void increaseDAPIndex(){
+		if ((dataDAP.Length-1) > indexDAP) {
+			indexDAP++;
+		}
+	}
+	public void increaseCANIndex(){
+		if ((dataCAN.Length-1) > indexDAP) {
+			indexCAN++;
+		}
+	}
+	public void increaseLIndex(){
+		if ((dataL.Length-1) > indexDAP) {
+			indexL++;
+		}
+	}
+	public void decreaseDAPIndex(){
+		if (indexDAP > 0) {
+			indexDAP++;
+		}
+	}
+	public void decreaseCANIndex(){
+		if (indexCAN > 0) {
+			indexCAN++;
+		}
+	}
+	public void decreaseLIndex(){
+		if (indexL > 0) {
+			indexL++;
+		}
+	}
 }
 
 
