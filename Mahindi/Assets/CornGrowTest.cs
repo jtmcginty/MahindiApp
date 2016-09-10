258a5c7f35a79f3bf1d8f489229979ee2b24e0de
tylerlybb <tylerlybb@gmail.com>
2016-09-09 21:21:10 -0600

    Adding corn animation test

---
+++ Mahindi/Assets/CornGrowTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornGrowTest : MonoBehaviour
+{
+	Animator anim;
+	int buttonHash = Animator.StringToHash("ButtonPress");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void AnimateNow()
+	{
+		anim.SetTrigger (buttonHash);
+	}
+}
---
+++ Mahindi/Assets/HarvestTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarvestTest : MonoBehaviour
+{
+	Animator anim;
+	int buttonHash = Animator.StringToHash("HarvestPress");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void AnimateNow()
+	{
+		anim.SetTrigger (buttonHash);
+	}
+}
