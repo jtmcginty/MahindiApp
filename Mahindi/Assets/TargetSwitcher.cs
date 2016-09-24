cebf4508f316db655812c08630ddb6d3bc4f1205
tylerlybb <tylerlybb@gmail.com>
2016-09-24 08:05:48 -0600

    Added a UI panel for animation buttons. Changed camera functionality so that each target is it's own method. Added a third target to the CameraSwitcher script.

--- Mahindi/Assets/TargetSwitcher.cs
+++ Mahindi/Assets/TargetSwitcher.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class TargetSwitcher : MonoBehaviour {
 public Transform target1;
 public Transform target2;
+public Transform target3;
 
 public void Switch() {
 	JoesScript follower = GetComponent<JoesScript>();
-	if (follower == null) Debug.LogError("Hey, where's the ExampleClass?!?");
+	if (follower == null) Debug.LogError("Hey, where's the JoesScript?!?");
 
 	if (follower.target == target1) follower.target = target2;
 	else follower.target = target1;
+	}
+public void Cam1() {
+	JoesScript follower = GetComponent<JoesScript>();
+	if (follower == null) Debug.LogError("Hey, where's JoesScript?!?");
+
+	follower.target = target1;
+	}
+public void Cam2() {
+	JoesScript follower = GetComponent<JoesScript>();
+	if (follower == null) Debug.LogError("Hey, where's JoesScript?!?");
+
+	follower.target = target2;
+	}
+public void Cam3() {
+	JoesScript follower = GetComponent<JoesScript>();
+	if (follower == null) Debug.LogError("Hey, where's JoesScript?!?");
+
+	follower.target = target3;
 	}
 }
