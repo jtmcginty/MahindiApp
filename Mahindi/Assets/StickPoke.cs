81c90c1203daf460d504388ea181622d1640eeb5
tylerlybb <tylerlybb@gmail.com>
2016-09-27 00:16:45 -0600

    Added a stick model and animation. Just start to get functionality.

---
+++ Mahindi/Assets/StickPoke.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickPoke : MonoBehaviour
+{
+	public Renderer rend;
+	Animator anim;
+	int buttonHash = Animator.StringToHash("Poke");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+		rend = GetComponent<Renderer>();
+		rend.enabled = false;
+	}
+
+	public void StickPokeNow()
+	{
+		rend.enabled = true;
+		anim.SetTrigger (buttonHash);
+	}
+}
