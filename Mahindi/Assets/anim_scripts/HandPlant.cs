4b3d212be886bb76a15d539be7ffe0299a264ee1
tylerlybb <tylerlybb@gmail.com>
2016-09-29 06:50:27 -0600

    Added a hand. This is really rough..

---
+++ Mahindi/Assets/anim_scripts/HandPlant.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandPlant : MonoBehaviour {
+	public Renderer rend;
+	Animator anim;
+	int buttonHash = Animator.StringToHash("AnimGo");
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rend = GetComponent<Renderer>();
+		rend.enabled = false;
+	}
+	
+	public void AnimateNow()
+	{
+		rend.enabled = true;
+		anim.SetTrigger (buttonHash);
+	}
+}
