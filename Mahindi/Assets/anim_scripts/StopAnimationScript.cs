eec25d616047f9a05f770d4e191bd3d09e635072
tylerlybb <tylerlybb@gmail.com>
2016-10-06 18:17:02 -0600

    Added some rain.. this is really really rough. I'm just experimenting.

---
+++ Mahindi/Assets/anim_scripts/StopAnimationScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopAnimationScript : MonoBehaviour {
+	Animator anim;
+	int buttonHash = Animator.StringToHash("StopAnimationParam");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void StopAnimationFunction()
+	{
+		anim.SetTrigger (buttonHash);
+	}
+}
